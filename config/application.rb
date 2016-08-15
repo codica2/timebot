7637005ce5f876556e97c1eca7e04e2c76bd9918
Alexander Bogomolov <alexbogomolov.kharkiv@gmail.com>
2016-08-15 12:40:29 +0300

    fix autoload_paths production issue

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,6 @@ module Timebot
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.autoload_paths << "#{config.root}/lib"
+    config.eager_load_paths << "#{config.root}/lib"
   end
 end
