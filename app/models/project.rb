592a3643b0fef78f9dce41ac1a4fd65d1cd95ffd
Alexander Bogomolov <alexbogomolov.kharkiv@gmail.com>
2016-08-15 12:59:25 +0300

    change Project#to_s method

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,8 +3,8 @@ class Project < ApplicationRecord
   MINIMUM_PROJECT_NAME_LENGTH = 4
 
   def to_s
-    string = "`#{self.id}`. *#{self.name}*"
-    string += ";\nAlias: *#{self.alias}*" if self.alias.present?
+    string = "#{self.id}. *#{self.name}*"
+    string += "; Alias: *#{self.alias}*" if self.alias.present?
     string
   end
 end
