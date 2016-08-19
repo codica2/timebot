9e50dbbc6f4cead3f831f25af5e29069f922bd4a
Volkov Sergey <volkovbro1997@gmail.com>
2016-08-19 11:00:46 +0300

    add yesterday to to time entries

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -8,9 +8,10 @@ class TimeEntry < ApplicationRecord
   validates :user_id, presence: true
 
   scope :today, ->{ where(date: Time.now.to_date) }
+  scope :yesterday, ->{ where(date: (1.day.ago.beginning_of_day..1.day.ago.end_of_day)) }
   scope :current_week, -> { where(date: (Time.now.beginning_of_week..Time.now.to_date)) }
   scope :last_week, ->{ where(date: (1.week.ago.beginning_of_week.to_date..1.week.ago.end_of_week.to_date)) }
-  scope :last_month, ->{ where(date: (Time.now.beginning_of_month.to_date..Time.now.to_date)) }
+  scope :current_month, ->{ where(date: (Time.now.beginning_of_month.to_date..Time.now.to_date)) }
 
   def description
     if self.details.present?
