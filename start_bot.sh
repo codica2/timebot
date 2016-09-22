2607839719271fe7430928aa809bf886fbbcf249
Alex Bogomolov <alexbogomolov.kharkiv@gmail.com>
2016-09-22 12:21:48 +0300

    changed pidfile in start_bot.sh

--- start_bot.sh
+++ start_bot.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-RAILS_ENV=production nohup rake start_bot > log/bot.log & echo $! > tmp/bot.pid
+RAILS_ENV=production nohup rake start_bot > log/bot.log & echo $! > tmp/pids/bot.pid
