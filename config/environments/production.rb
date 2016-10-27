924bbb51c6daf229ba47d7c69e0635cd0d0950e3
Alex Bogomolov <alexbogomolov.kharkiv@gmail.com>
2016-10-27 12:45:25 +0300

    send ActiveAdmin emails in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,10 +56,12 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "timebot_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { host: 'http://timebot.codica.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
