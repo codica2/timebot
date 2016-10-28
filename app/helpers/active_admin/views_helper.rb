4d7ad1d43e34ad0438bdf24514aab2782e33111a
Alex Bogomolov <alexbogomolov.kharkiv@gmail.com>
2016-10-28 17:39:30 +0300

    fix Projects panel in ActiveAdmin Time Entries

--- app/helpers/active_admin/views_helper.rb
+++ app/helpers/active_admin/views_helper.rb
@@ -2,10 +2,10 @@
 module ActiveAdmin
   module ViewsHelper
     def projects_by_user
-      projects = collection.map(&:project).uniq
+      projects = time_entries.map(&:project).uniq
 
       projects_info = projects.map do |project|
-        entries = collection.select { |entry| entry.project_id == project.id }
+        entries = time_entries.select { |entry| entry.project_id == project.id }
         time = entries.map(&:minutes).select(&:present?).inject(&:+)
         hours   = time / 60
         minutes = time % 60
@@ -22,10 +22,10 @@ module ActiveAdmin
     end
 
     def users_by_project
-      users = collection.map(&:user).uniq
+      users = time_entries.map(&:user).uniq
 
       users_info = users.map do |user|
-        entries = collection.select { |entry| entry.user_id == user.id }
+        entries = time_entries.select { |entry| entry.user_id == user.id }
         time = entries.map(&:minutes).select(&:present?).inject(&:+)
         hours   = time / 60
         minutes = time % 60
@@ -59,6 +59,24 @@ module ActiveAdmin
 
     def pluralize_time(hours, minutes)
       "#{pluralize(hours, 'hour')} #{pluralize(minutes, 'minute')}"
+    end
+
+    def time_entries
+      if @time_entries_collection
+        @time_entries_collection
+      else
+        @time_entries_collection = TimeEntry.all
+        if (user_id = params.dig(:q, :user_id_eq))
+          @time_entries_collection = @time_entries_collection.where(user_id: user_id)
+        end
+        if (project_id = params.dig(:q, :project_id_eq))
+          @time_entries_collection = @time_entries_collection.where(project_id: project_id)
+        end
+        if (date_gteq = params.dig(:q, :date_gteq_date)) && (date_lteq = params.dig(:q, :date_lteq_date))
+          @time_entries_collection = @time_entries_collection.where(['date BETWEEN ? AND ?', date_gteq, date_lteq])
+        end
+        @time_entries_collection
+      end
     end
   end
 end
