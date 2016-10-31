a8828a9f67657db8309df403bdd14d86c7cc69cd
Alex Bogomolov <alexbogomolov.kharkiv@gmail.com>
2016-10-31 13:58:41 +0200

    make year optional in dates in update timesheet and set absence

--- lib/helper.rb
+++ lib/helper.rb
@@ -5,8 +5,12 @@ module Helper
     launch_date && launch_date > start_date ? launch_date : start_date
   end
 
-  def build_date(day, month, year)
-    year = year.to_i + 2000 if year.to_i < 100
-    Date.new(year.to_i, month.to_i, day.to_i)
+  def parse_date(date_string)
+    match_data = date_string.match(/^(\d?\d)\.(\d?\d)(?:\.(\d?\d?\d\d))?$/)
+    day   = match_data[1].to_i
+    month = match_data[2].to_i
+    year  = match_data[3] ? match_data[3].to_i : Time.zone.today.year
+    year  = year + 2000 if year < 100
+    Date.new(year, month, day)
   end
 end
