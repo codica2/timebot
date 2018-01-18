c24f345ef1c9535a69db566613efff96b3742021
Sergey Volkov <volkovbro1997@gmail.com>
2018-01-18 17:40:13 +0200

    [Fix] Aliases

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,9 +4,22 @@ class Project < ApplicationRecord
 
   has_many :time_entries
 
+  validates :name, :alias, uniqueness: { case_sensitive: false }
+
+  before_validation :generate_alias
+
   def to_s
     string = "#{id}. *#{name}*"
     string += "; Alias: *#{self.alias}*" if self.alias.present?
     string
   end
+
+  private
+
+  def generate_alias
+    return if self.alias.present?
+
+    self.alias = name.downcase.tr(' ', '-')
+  end
+
 end
