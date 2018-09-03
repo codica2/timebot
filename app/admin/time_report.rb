52c99f9f332c6e68507b0c9fd7f678e20a42b516
Eugeny Khlopin <ekhlopin.codica@gmail.com>
2018-09-03 13:12:57 +0300

    [Fix] Rename report pages

--- app/admin/time_report.rb
+++ app/admin/time_report.rb
@@ -1,40 +1,75 @@
 # frozen_string_literal: true
-ActiveAdmin.register TimeEntry, as: 'Time Report' do
+ActiveAdmin.register_page 'Time Reports' do
   menu parent: 'Reports'
 
-  filter :user, as: :select, collection: proc { User.active.order(:name).map { |user| [user.name, user.id.to_s]} }, input_html: { multiple: true }
-  filter :project, as: :select, collection: proc { Project.order(:name).map { |prj| [prj.name, prj.id.to_s]} }, input_html: { multiple: true }
-  filter :details_contains, as: :text, input_html: { spellcheck: false }
-
-  index do
-    column :project
-    column :details
-    column :collaborators
-    column :created_at
-    column :trello_labels do |f|
-      formatted_labels(f.trello_labels)[:labels]
-    end
-    column :estimate do |f|
-      formatted_labels(f.trello_labels)[:estimate]
-    end
-    column :total_time
-    column :status do |f|
-      f.trello_list_name
+  controller do
+    def index
+      unless params.dig(:q, :date_gteq_date) && params.dig(:q, :date_lteq_date)
+        redirect_to admin_time_reports_url(q: { date_gteq_date: Date.today.beginning_of_week,
+                                          date_lteq_date: Date.today.end_of_week })
+      end
     end
   end
 
-  controller do
-    def scoped_collection
-      tickets = end_of_association_chain.where.not(ticket: nil).to_a.uniq(&:ticket)
-      details = end_of_association_chain.where(ticket: nil).to_a.uniq(&:details)
-      entries = (tickets + details).sort
-      entries = TimeEntry.where(id: entries.map(&:id))
+  content title: proc { I18n.t('active_admin.dashboard') } do
+    start_date = Date.parse(params.dig(:q, :date_gteq_date))
+    end_date   = Date.parse(params.dig(:q, :date_lteq_date))
 
-      start_date = params.dig(:q, :date_gteq_date)&.to_date
-      end_date   = params.dig(:q, :date_lteq_date)&.to_date
+    h2 "Statistics for #{start_date.strftime('%b %e, %Y')} - #{end_date.strftime('%b %e, %Y')}"
 
-      (start_date && end_date) ? entries.in_interval(start_date, end_date) : entries
+    Team.all.each do |team|
+      h3 "Project by Team #{team.name}"
+      table(class: 'index_table report') do
+        thead do
+          th 'Project'
+          th 'Ticket'
+          th 'Labels'
+          th 'Users Worked on'
+          th 'Time'
+          th 'Status'
+        end
+        tbody do
+          team_project_time_entries(team.id).each_with_index do |company_time_entry, _index|
+            tr(class: 'even report_by_ticket__project-name', 'data-id': company_time_entry.first.id) do
+              td do
+                b company_time_entry.first.name
+              end
+              td
+              td
+              td
+              td do
+                b time_worked(company_time_entry)
+              end
+              td
+            end
+            tbody('data-by-ticket-project-id': company_time_entry.first.id, style: 'display: none') do
+              company_time_entry.last.group_by(&:details).each do |detail_time_entry|
+                tr do
+                  td
+                  td detail_time_entry.first
+                  td formatted_labels(detail_time_entry.last.map(&:trello_labels))[:labels]
+                  td user_names(detail_time_entry)
+                  td time_worked(detail_time_entry)
+                  td detail_time_entry.flatten.last.trello_list_name
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 
+  sidebar :filters do
+    form class: 'filter_form', method: '/get', action: '/admin/time_reports' do
+      div class: 'buttons' do
+        button type: 'submit' do
+          'Filter'
+        end
+        a class: 'clear_filters_btn', href: '#' do
+          'Clear Filters'
+        end
+      end
     end
   end
 end
