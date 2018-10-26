08c70da66eb9b3cdbfe8dae1bd31f2b142802bad
Igor Tikhonenko <itikhonenko.codica@gmail.com>
2018-10-26 16:42:44 +0300

    [Feature] User created at date format

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,4 +3,12 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :uid, :created_at, :is_active, :role
   belongs_to :team
+
+  def created_at
+    {
+      date: object.created_at.strftime("%d %b, %Y"),
+      time: object.created_at.strftime("%H:%M")
+    }
+  end
+
 end
